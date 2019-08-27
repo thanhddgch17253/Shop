7f267a16698e86d9b3984a55fa90d13dc033b5b9
Do Duc Thanh <thanhddgch17253@fpt.edu.vn>
2019-08-27 14:16:49 +0700

    1

---
+++ indext.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<!--Code By Webdevtrick ( https://webdevtrick.com )-->
+<html lang="en" >
+ 
+<head>
+  <meta charset="UTF-8">
+  <title>3D Image Cube Carousel | Webdevtrick.com</title>
+  <link href="https://fonts.googleapis.com/css?family=Staatliches&display=swap" rel="stylesheet">
+  <link href="style.css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+  
+
+
+</head>
+
+<body> 
+<div class="login">
+            <a href="login1.php"><button class="btn">Login For Serior Staff</button></a>
+            <a href="login2.php"><button class="btn">Login For Store</button></a>
+  </div>
+
+
+<div class="container">
+  <h1 class="h1">ATN Company</h1>
+  <div class="carousel" data-slide="1">
+    <div class="slides">
+      <img class="sidles1" src="https://lepin.com.vn/wp-content/uploads/2018/06/98f9c50af462b1dba96e000c336fbc37.jpeg" />
+    </div>
+    <div class="slides">
+      <img class="sidles1" src="https://ae01.alicdn.com/kf/HTB1uSMmfXYM8KJjSZFuq6Af7FXau/Japanese-anime-figures-Gundam-SD-BB-Full-Armor-Unicorn-action-figure-plastic-model-kits-toys.jpg_640x640.jpg" />
+    </div>
+    <div class="slides">
+      <img class="sidles1" src="https://hangdepgiare.club/hinh-anh/1554470256__4a8408b1bff0bdcf3ca12f732bb7959d/thong-tin-cac-chung-loai-mo-hinh-lap-rap-go-3d-giu-uy-tin-nhat-tai-ho-chi-minh.jpg" />
+    </div>
+    <div class="slides">
+      <img class="sidles1" src="https://pplay.vn/media/catalog/product/cache/1/image/485x440/9df78eab33525d08d6e5fb8d27136e95/1/0/10875_alt1.jpg" />
+    </div>
+  </div>
+  <div class="next">NEXT &#8680;</div>
+  <div class="prev">&#8678; PREV</div>
+</div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script  src="function.js"></script>
+</body>
+
+</html>
