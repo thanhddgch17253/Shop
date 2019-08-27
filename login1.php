0a69d2882eb9dbb8f29dbeba16bbb4b6047f7fb9
Do Duc Thanh <thanhddgch17253@fpt.edu.vn>
2019-08-27 16:50:58 +0700

    1

--- login1.php
+++ login1.php
@@ -8,59 +8,75 @@
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
       <link rel="stylesheet" href="./assets/css/style.css">
   
+      <style>
+        h1 {
+            text-align: center;
+            font-size: 450%;
+            color: rgb(255, 63, 78);
+        }
+        
+        form {
+            margin-top: 10%;
+            margin-left: 30%;
+            margin-right: 30%;
+        }
+        
+        .login {
+            margin-left: 20%;
+        }
+        body{
+            background-color: rgb(186, 242, 167);
+        }
+        form{
+            color: rgb(255, 63, 78);
+        }
+    </style>
 </head>
 <body>
-  <div class="login-wrap">
-  <div class="login-html">
-    <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
-    <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
-    <div class="login-form">
-      <form class="sign-in-htm" action="./api/user/login.php" method="GET">
-        <div class="group">
-          <label for="user" class="label">Username</label>
-          <input id="username" name="username" type="text" class="input">
+<header>
+        <a href="index.php"><button class="btn btn-danger">Back</button></a>
+    </header>
+    <h1>Serior Staff Login</h1>
+    <form action="login1.php" method="POST">
+        <div class="form-group">
+            <label for="username">User name:</label>
+            <input type="text" class="form-control" name="username">
+         </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" name="pass">
         </div>
-        <div class="group">
-          <label for="pass" class="label">Password</label>
-          <input id="password" name="password" type="password" class="input" data-type="password">
         </div>
-        <div class="group">
-          <input id="check" type="checkbox" class="check" checked>
-          <label for="check"><span class="icon"></span> Keep me Signed in</label>
-        </div>
-        <div class="group">
-          <input type="submit" class="button" value="Sign In">
-        </div>
-        <div class="hr"></div>
-        <div class="foot-lnk">
-          <a href="#forgot">Forgot Password?</a>
-        </div>
-      </form>
-      <form class="sign-up-htm" action="./api/user/signup.php" method="POST">
-        <div class="group">
-          <label for="user" class="label">Username</label>
-          <input id="username" name="username" type="text" class="input">
-        </div>
-        <div class="group">
-          <label for="pass" class="label">Password</label>
-          <input id="password" name="password" type="password" class="input" data-type="password">
-        </div>
-        <div class="group">
-          <label for="pass" class="label">Confirm Password</label>
-          <input id="pass" type="password" class="input" data-type="password">
-        </div>
-        <div class="group">
-          <input type="submit" class="button" value="Sign Up">
-        </div>
-        <div class="hr"></div>
-        <div class="foot-lnk">
-          <label for="tab-1">Already Member?</a>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-  
-  
+        <button type="submit" class="btn btn-danger" name="login">Login</button>
+    </form> 
 </body>
+<?php 
+    require_once "connect.php";
+    if(!isset($_POST['login']))
+    {
+
+    }
+    else{
+        $sql = "SELECT * FROM manage where username = :username and pass = :pass";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute
+        (
+            array(
+                'username' => $_POST['username'],
+                'pass' => $_POST['pass'],
+            )
+        );
+        $count = $stmt->rowCount();
+        if($count > 0)
+        {   
+            $_SESSION['username'] = $_POST['username'];
+           echo'<script language="javascript">alert("Login sucessfully"); window.location="Seniorstaff.php";</script>';
+            
+        }
+        else
+        {
+            echo'<script language="javascript">alert("Uesername or password incorrect");</script>';
+        }
+    }
+?>
 </html>
